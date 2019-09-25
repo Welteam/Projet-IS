d9d8366b10f27b40598847bee986f65ec55bb064
Xsims <romeoprosecco@gmailcom>
2019-09-25 16:28:39 +0200

    Update Rapport.pdf for 1.1

---
+++ cmake-build-debug/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xsims/CLionProjects/Projet-IS/test
+# Build directory: /home/xsims/CLionProjects/Projet-IS/cmake-build-debug/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("shared")
+subdirs("client")
---
+++ cmake-build-debug/test/client/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/xsims/CLionProjects/Projet-IS/test/client
+# Build directory: /home/xsims/CLionProjects/Projet-IS/cmake-build-debug/test/client
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_client_dummy "/home/xsims/CLionProjects/Projet-IS/cmake-build-debug/test/client/test_client_dummy")
+set_tests_properties(test_client_dummy PROPERTIES  _BACKTRACE_TRIPLES "/home/xsims/CLionProjects/Projet-IS/test/CMakeLists.txt;77;add_test;/home/xsims/CLionProjects/Projet-IS/test/client/CMakeLists.txt;3;add_custom_test;/home/xsims/CLionProjects/Projet-IS/test/client/CMakeLists.txt;0;")

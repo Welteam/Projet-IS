99fa9cf708dfa9501db246b2896464a3f97102da
Xsims <romeoprosecco@gmailcom>
2019-09-25 16:28:39 +0200

    Revert "Update Rapport.pdf for 1.1"
    This reverts commit d9d8366b10f27b40598847bee986f65ec55bb064.

--- cmake-build-debug/extern/dia2code/CMakeFiles/dia2code.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for dia2code.
-# This may be replaced when dependencies are built.
--- cmake-build-debug/test/shared/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xsims/CLionProjects/Projet-IS/test/shared
-# Build directory: /home/xsims/CLionProjects/Projet-IS/cmake-build-debug/test/shared
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_shared_dummy "/home/xsims/CLionProjects/Projet-IS/cmake-build-debug/test/shared/test_shared_dummy")
-set_tests_properties(test_shared_dummy PROPERTIES  _BACKTRACE_TRIPLES "/home/xsims/CLionProjects/Projet-IS/test/CMakeLists.txt;77;add_test;/home/xsims/CLionProjects/Projet-IS/test/shared/CMakeLists.txt;2;add_custom_test;/home/xsims/CLionProjects/Projet-IS/test/shared/CMakeLists.txt;0;")

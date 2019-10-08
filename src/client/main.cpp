5c93c36d6a7b44ca271bbe96d9303bd8e0db5203
Xsims <romeoprosecco@gmailcom>
2019-10-08 12:05:07 +0200

    Fix main.cpp

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -25,17 +25,17 @@ int main(int argc,char* argv[])
 }
 
 // TODO 1.0 : Create the screen title with a background image, buttons to launch/exit the game
-SFMLWindows titleScreen = new SFMLWindows ...;
+//SFMLWindows titleScreen = new SFMLWindows ...;
 
 /* TODO 2.0 : Game screen containing status bar(LP, nb of units, etc.. for each player),
  * TODO 2.1 : a layer of the static map,
  * TODO 2.2 : interaction bottom bar with buttons for settings (tactical render of the map, sound...). */
-SFMLWIndows gameScreen = new SFMLWindows ...;
+//SFMLWIndows gameScreen = new SFMLWindows ...;
 
 // TODO 3.0 Read txt file create two players with initial number of character
-initPlayers(){
+/*initPlayers(){
     new Map(dile.txt)
     new player(Map.units1, Map.tower1, Map.apparitionArea1)
     ....
-}
+}*/
 

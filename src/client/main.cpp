2d4d985f498d5aefd59a6c564541be5e0ab2b623
Xsims <romeoprosecco@gmailcom>
2019-10-08 11:40:04 +0200

    dia generation V1.0

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -23,3 +23,19 @@ int main(int argc,char* argv[])
 
     return 0;
 }
+
+// TODO 1.0 : Create the screen title with a background image, buttons to launch/exit the game
+SFMLWindows titleScreen = new SFMLWindows ...;
+
+/* TODO 2.0 : Game screen containing status bar(LP, nb of units, etc.. for each player),
+ * TODO 2.1 : a layer of the static map,
+ * TODO 2.2 : interaction bottom bar with buttons for settings (tactical render of the map, sound...). */
+SFMLWIndows gameScreen = new SFMLWindows ...;
+
+// TODO 3.0 Read txt file create two players with initial number of character
+initPlayers(){
+    new Map(dile.txt)
+    new player(Map.units1, Map.tower1, Map.apparitionArea1)
+    ....
+}
+

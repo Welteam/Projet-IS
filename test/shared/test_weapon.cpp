54fcd5812762fa7d4fa93be1788add07c9816546
Xsims <romeoprosecco@gmailcom>
2019-10-15 15:07:13 +0200

    fixing : Add empty constructor to state.dia, unit test and cpp made for Weapon

--- test/shared/test_weapon.cpp
+++ test/shared/test_weapon.cpp
@@ -1,3 +1,4 @@
+#include "../../src/shared/state.h"
 
 #include <boost/test/unit_test.hpp>
 
@@ -6,25 +7,58 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
   BOOST_CHECK(1);
 }
 
-BOOST_AUTO_TEST_CASE(TestExemple)
+BOOST_AUTO_TEST_CASE(TestWeaponGettersSettersConstrutor)
 {
-  /*
-  {
-    Exemple ex {};
-    BOOST_CHECK_EQUAL(ex.x, 0);
-    ex.setX(21);
-    BOOST_CHECK_EQUAL(ex.x, 21);
-  }
+    { // GET & SET pm
+       state::Weapon weapon;
+       BOOST_CHECK_EQUAL(weapon.getPm(), 0);
+       weapon.setPm(5);
+       BOOST_CHECK_EQUAL(weapon.getPm(), 5);
+    }
 
-  {
-    Exemple ex {};
-    ex.setX(21);
+    { // GET & SET damage
+        state::Weapon weapon;
+        BOOST_CHECK_EQUAL(weapon.getDamage(), 0);
+        weapon.setDamage(5);
+        BOOST_CHECK_EQUAL(weapon.getDamage(), 5);
+    }
+
+    { // GET & SET rangeMin & rangeMax
+        state::Weapon weapon;
+        BOOST_CHECK_EQUAL(weapon.getRangeMin(), 0);
+        BOOST_CHECK_EQUAL(weapon.getRangeMax(), 0);
+        weapon.setRangeMax(5);
+        BOOST_CHECK_EQUAL(weapon.getRangeMax(), 5);
+    }
+
+    { // GET & SET damageAreaMax
+        state::Weapon weapon;
+        BOOST_CHECK_EQUAL(weapon.getDamageAreaMax(), 0);
+        weapon.setDamageAreaMax(5);
+        BOOST_CHECK_EQUAL(weapon.getDamageAreaMax(), 5);
+    }
+
+    { // GET & SET Direction
+        state::Weapon weapon;
+        BOOST_CHECK_EQUAL(weapon.getDirection(), 0);
+        weapon.setDirection(static_cast<state::DirectionType>(4));
+        BOOST_CHECK_EQUAL(weapon.getDirection(), 4);
+    }
+
+    { // GET & SET Direction
+        state::Weapon weapon{5, 50, 0, 5, 5, static_cast<state::DirectionType>(4)};
+        BOOST_CHECK_EQUAL(weapon.getPm(), 5);
+        BOOST_CHECK_EQUAL(weapon.getDamage(), 50);
+        BOOST_CHECK_EQUAL(weapon.getRangeMin(), 0);
+        BOOST_CHECK_EQUAL(weapon.getRangeMax(), 5);
+        BOOST_CHECK_EQUAL(weapon.getDamageAreaMax(), 5);
+        BOOST_CHECK_EQUAL(weapon.getDirection(), 4);
+    }
+
+
+  /*
     BOOST_CHECK_LE(ex.x, 32); // Less than equal
     BOOST_CHECK_GT(ex.x, 11); // Greater than equl
-  }
-   constructor, cast, assert equal attempt,
-
-
   */
 }
 

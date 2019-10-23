a4b59e18344eadb64ddc63567110b5a437eeb4ac
Welteam <main@localhost.localdomain>
2019-10-23 22:57:49 +0200

    New level up system.

--- src/shared/state/Weapon.cpp
+++ src/shared/state/Weapon.cpp
@@ -3,16 +3,18 @@
 
 namespace state {
 
-    Weapon::Weapon() : pm(0), damage(0), rangeMin(0), rangeMax(0), damageAreaMax(0) {
+    Weapon::Weapon() : level{0}, pm{0}, damage{0}, rangeMin{0}, rangeMax{0}, damageAreaMax{0}, direction{DirectionType::FULL} {
 
     }
 
-    Weapon::Weapon(int pm, int damage, int rangeMin, int rangeMax, int damageAreaMax) {
+    Weapon::Weapon(int level,int pm, int damage, int rangeMin, int rangeMax, int damageAreaMax, DirectionType direction) {
+        this->level = level;
         this->pm = pm;
         this->damage = damage;
         this->rangeMin = rangeMin;
         this->rangeMax = rangeMax;
         this->damageAreaMax = damageAreaMax;
+        this->direction = direction;
     }
 
     int Weapon::getPm() const {
@@ -50,6 +52,49 @@ namespace state {
     void Weapon::setDamageAreaMax(int damageAreaMax) {
         this->damageAreaMax = damageAreaMax;
     }
+
+    int Weapon::getDirection() const {
+        return this->direction;
+    }
+
+    int Weapon::getLevel() const {
+        return this->level;
+    }
+
+    void Weapon::levelUp() {
+        int currentLevel = this->getLevel();
+        switch(currentLevel){
+            case 0:
+                this->level = 1; // Shotgun
+                this->pm = 5;
+                this->damage = 60;
+                this->rangeMin = 0;
+                this->rangeMax = 3;
+                this->damageAreaMax = 0;
+                this->direction = DirectionType::CROSS;
+                break;
+            case 1:
+                this->level = 2; // Sniper
+                this->pm = 4;
+                this->damage = 40;
+                this->rangeMin = 0;
+                this->rangeMax = 8;
+                this->damageAreaMax = 0;
+                this->direction = DirectionType::FULL;
+                break;
+            case 2:
+                this->level = 3; // Bazooka
+                this->pm = 4;
+                this->damage = 50;
+                this->rangeMin = 3;
+                this->rangeMax = 7;
+                this->damageAreaMax = 2;
+                this->direction = DirectionType::OCTOPUS;
+                break;
+        }
+
+    }
+
 
 }
 

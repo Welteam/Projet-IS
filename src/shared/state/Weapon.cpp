c51e56d56d3cce494cc95398bff30733339db834
Welteam <main@localhost.localdomain>
2019-11-12 17:58:39 +0100

    Improved weapon class

--- src/shared/state/Weapon.cpp
+++ src/shared/state/Weapon.cpp
@@ -3,123 +3,53 @@
 
 namespace state {
 
-    // TODO: Remove setters and add setLevel method
-
     Weapon::Weapon() {
-
-    }
-
-    Weapon::Weapon(int level,int lpMax, int pm, int damage, int rangeMin, int rangeMax, int damageAreaMax, DirectionType direction) {
-        this->level = level;
-        this->lpMax = lpMax;
-        this->pmMax = pm;
-        this->damage = damage;
-        this->rangeMin = rangeMin;
-        this->rangeMax = rangeMax;
-        this->damageAreaMax = damageAreaMax;
-        this->direction = direction;
+        // Value is too long so dia2code cannot declare it as a default value
+        this->direction = {DirectionType::FULL, DirectionType::CROSS, DirectionType::FULL, DirectionType::OCTOPUS};
     }
 
     int Weapon::getPmMax() const {
-        return this->pmMax;
+        return this->pmMax[this->level];
     }
 
     int Weapon::getDamage() const {
-        return this->damage;
+        return this->damage[this->level];
     }
 
     int Weapon::getRangeMin() const {
-        return this->rangeMin;
+        return this->rangeMin[this->level];
     }
 
     int Weapon::getRangeMax() const {
-        return this->rangeMax;
+        return this->rangeMax[this->level];
     }
 
     int Weapon::getDamageAreaMax() const {
-        return this->damageAreaMax;
+        return this->damageAreaMax[this->level];
     }
 
     DirectionType Weapon::getDirection() const {
-        return this->direction;
+        return this->direction[this->level];
+    }
+
+    int Weapon::getLpMax() const {
+        return this->lpMax[this->level];
     }
 
     int Weapon::getLevel() const {
         return this->level;
     }
 
+    // TODO: add error
     void Weapon::levelUp() {
-        switch(this->level){
-            case 0:
-                this->level = 1; // Shotgun
-                this->lpMax = 120;
-                this->pmMax = 5;
-                this->damage = 60;
-                this->rangeMin = 0;
-                this->rangeMax = 3;
-                this->damageAreaMax = 2;
-                this->direction = DirectionType::CROSS;
-                break;
-            case 1:
-                this->level = 2; // Sniper
-                this->lpMax = 140;
-                this->pmMax = 4;
-                this->damage = 40;
-                this->rangeMin = 0;
-                this->rangeMax = 8;
-                this->damageAreaMax = 0;
-                this->direction = DirectionType::FULL;
-                break;
-            case 2:
-                this->level = 3; // Bazooka
-                this->lpMax = 160;
-                this->pmMax = 4;
-                this->damage = 50;
-                this->rangeMin = 3;
-                this->rangeMax = 7;
-                this->damageAreaMax = 3;
-                this->direction = DirectionType::OCTOPUS;
-                break;
+        if (this->level < 3) {
+            this->level += 1;
         }
-
-    }
-
-    int Weapon::getLpMax() const {
-        return this->lpMax;
     }
 
     void Weapon::setLevel(int level) {
-        switch(level){
-            case 1:
-                this->level = 1; // Shotgun
-                this->lpMax = 120;
-                this->pmMax = 5;
-                this->damage = 60;
-                this->rangeMin = 0;
-                this->rangeMax = 3;
-                this->damageAreaMax = 0;
-                this->direction = DirectionType::CROSS;
-                break;
-            case 2:
-                this->level = 2; // Sniper
-                this->lpMax = 140;
-                this->pmMax = 4;
-                this->damage = 40;
-                this->rangeMin = 0;
-                this->rangeMax = 8;
-                this->damageAreaMax = 0;
-                this->direction = DirectionType::FULL;
-                break;
-            case 3:
-                this->level = 3; // Bazooka
-                this->lpMax = 160;
-                this->pmMax = 4;
-                this->damage = 50;
-                this->rangeMin = 3;
-                this->rangeMax = 7;
-                this->damageAreaMax = 2;
-                this->direction = DirectionType::OCTOPUS;
-                break;
+        if (level <= 3) {
+            this->level = level;
         }
     }
 
